2bc269b905a3190c21595fe582538b0cad1946b2
RosiCoimbra <rscoimbra2015@gmail.com>
2023-03-14 19:08:34 -0300

    DetailsError

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -11,6 +11,8 @@ export class PokeListComponent implements OnInit{
   private setAllPokemons: any;
   public getAllPokemons: any;
 
+  public apiError: boolean = false;
+
   constructor(
     private pokeApiService: PokeApiService
   ){ }
@@ -19,7 +21,10 @@ export class PokeListComponent implements OnInit{
     this.pokeApiService.apiListAllPokemons.subscribe(
        res => {
         this.setAllPokemons = res.results;
-        this.getAllPokemons = res.setAllPokemons;
+        this.getAllPokemons = this.setAllPokemons;
+       },
+       error => {
+         this.apiError = true;
        }
     )
   }
